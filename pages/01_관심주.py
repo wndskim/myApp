18f34801a229406ce6b8d6e148648fccea6f507d
wndskim <wndskim01@gmail.com>
2023-03-24 17:41:06 +0900

    Update 01_관심주.py

--- pages/01_관심주.py
+++ pages/01_관심주.py
@@ -1,20 +1,22 @@
 import streamlit as st
 from pykrx import stock
 import pandas as pd
+from datetime import date
 
 st.write('관심주')
+조회일=date.today()
+조회일=str(조회일).replace('-','')
 
 chk00=st.sidebar.checkbox('코스피200 보기',value=False)
 if chk00:
+    st.text('코스피200 조회일 : '+조회일)
+    
     종목s=[]
     티커s=stock.get_index_portfolio_deposit_file('1028')
     for 티커 in 티커s:
         종목s.append(stock.get_market_ticker_name(티커))
     _df1=pd.DataFrame(list(zip(티커s, 종목s)), columns=['티커', '종목'])
-
-    st.dataframe(_df1)
-
-    _df2=stock.get_market_ohlcv('20230324')
+    _df2=stock.get_market_ohlcv(조회일)
     _df2=_df2[_df2.index.isin(티커s)]
 
     df=pd.merge(_df1, _df2, on='티커')
