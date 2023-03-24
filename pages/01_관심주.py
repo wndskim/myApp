d9945fa8567d7f78ddb789fa7e052e85e3c75049
wndskim <wndskim01@gmail.com>
2023-03-24 17:01:26 +0900

    Update 01_관심주.py

--- pages/01_관심주.py
+++ pages/01_관심주.py
@@ -1,6 +1,10 @@
 import streamlit as st
+from pykrx import stock
 
 st.write('관심주')
 
-
+chk00=st.sidebar.checkbox('코스피200 보기',value=False)
+if chk00:
+    티커s=stock.get_index_portfolio_deposit_file('1028')
+    st.write(티커s)
 
