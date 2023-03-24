d431fe8398f4d9ebb4a06d3c730752161d619e03
wndskim <wndskim01@gmail.com>
2023-03-24 11:57:49 +0900

    Update MSIS.py

--- MSIS.py
+++ MSIS.py
@@ -6,7 +6,9 @@ def main():
     st.header('My Stock Inquiry System(MSIS)')
 
     chk01=st.sidebar.checkbox('미연방은행(FRED) 주요지표 보기')
+    chk02=st.sidebar.checkbox('주요 뉴스 보기')
     if chk01: st.text('미연방은행 주요 지표')
+    if chk02: st.text('주요 뉴스 보기')
 
     return
 
