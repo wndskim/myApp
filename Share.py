e1f439070930284c29f0b42096b101e09e5b6ad2
wndskim <wndskim01@gmail.com>
2023-03-24 12:23:41 +0900

    Update Share.py

--- Share.py
+++ Share.py
@@ -1,15 +1,8 @@
 import streamlit as st
-from datetime import date, timedelta
+from datetime import timedelta
 
 def get_date(기준일, delta):
     return (기준일 - timedelta(days=delta)).strftime("%Y-%m-%d")
 
-조회일=date.today()
 
-st.write(조회일)
-
-시작일=str(get_date(조회일, 20)).replace('-','')  # 조회일로부터 20일전 부터 데이타 가져오기
-종료일=str(조회일).replace('-','')
-
-st.write(시작일,종료일)
 
