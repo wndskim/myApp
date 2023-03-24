9e1c192c57ed6bc641e2e2b30f7925d96b4df6db
wndskim <wndskim01@gmail.com>
2023-03-24 17:23:30 +0900

    Update 01_관심주.py

--- pages/01_관심주.py
+++ pages/01_관심주.py
@@ -5,13 +5,17 @@ st.write('관심주')
 
 chk00=st.sidebar.checkbox('코스피200 보기',value=False)
 if chk00:
+    종목s=[]
     티커s=stock.get_index_portfolio_deposit_file('1028')
+    for 티커 in 티커s:
+        종목s.append(stock.get_market_ticker_name(티커))
     st.write(티커s)
+    st.write(종목s)
 
-    df=stock.get_market_ohlcv('20230324')
+    # df=stock.get_market_ohlcv('20230324')
 
-    df = df[df.index.isin(티커s)]
+    # df = df[df.index.isin(티커s)]
 
-    st.write(len(df),'건')
-    st.dataframe(df)
+    # st.write(len(df),'건')
+    # st.dataframe(df)
 
