88a1f09626315f7d597e385649ae5c656555dc21
wndskim <wndskim01@gmail.com>
2023-03-26 10:27:34 +0900

    Update 01_관심주.py

--- pages/01_관심주.py
+++ pages/01_관심주.py
@@ -7,33 +7,28 @@ st.write('관심주')
 
 chk00=st.sidebar.checkbox('코스피200 보기',value=False)
 if chk00:
-
-    st.session_state['status']='first'
-
-
     col1,col2=st.columns([1,5])
     with col1:
         조회일=st.date_input('조회일', date.today())
-        # 조회일=date.today()
         조회일=str(조회일).replace('-','')
         container=st.container()
     with col2:
-        if st.session_state.status=='first':
-            종목s=[]
-            티커s=stock.get_index_portfolio_deposit_file('1028')
-            for 티커 in 티커s:
-                종목s.append(stock.get_market_ticker_name(티커))
-            _df1=pd.DataFrame(list(zip(티커s, 종목s)), columns=['티커', '종목'])
-            _df2=stock.get_market_ohlcv(조회일)
-            _df2=_df2[_df2.index.isin(티커s)]
+        종목s=[]
+        티커s=stock.get_index_portfolio_deposit_file('1028')
+        for 티커 in 티커s:
+            종목s.append(stock.get_market_ticker_name(티커))
+        _df1=pd.DataFrame(list(zip(티커s, 종목s)), columns=['티커', '종목'])
+        _df2=stock.get_market_ohlcv(조회일)
+        _df2=_df2[_df2.index.isin(티커s)]
 
-            df=pd.merge(_df1, _df2, on='티커')
-            df.sort_values(by='등락률', ascending=False, inplace=True)
+        df=pd.merge(_df1, _df2, on='티커')
+        df.sort_values(by='등락률', ascending=False, inplace=True)
 
-            st.write('코스피200',len(df),'건')
-            st.dataframe(df)
+        df.reset_index(inplace=True)
 
-            st.session_statestatus='코스피200'
+        st.write('코스피200',len(df),'건')
+        st.dataframe(df)
 
-            종목=container.selectbox('선택', 종목s)
-            container.text(종목)
+
+        종목=container.selectbox('선택', 종목s)
+        container.text(종목)
