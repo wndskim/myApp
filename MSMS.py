d399ddadeddf79c78c7de2960bd8ffbcf7d1ad31
wndskim <wndskim01@gmail.com>
2023-03-26 15:31:13 +0900

    Create MSMS.py

---
+++ MSMS.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+import Display
+
+
+def main():
+
+    st.header('My Stock Management System(MSMS)')
+
+    chk00=st.sidebar.checkbox('시장 지표 보기')
+    chk01=st.sidebar.checkbox('미연방은행(FRED) 주요지표 보기')
+    chk02=st.sidebar.checkbox('주요 뉴스 보기')
+    if chk00: Display.시장지표보기()
+    if chk01: Display.연방은행주요지표보기()
+    if chk02: Display.주요뉴스보기()
+
+    return
+
+
+
+#####################################################
+##### Main ##########################################
+#####################################################
+if __name__ == '__main__':
+    main()
