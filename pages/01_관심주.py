4fabdcdf6e1c259fee7f6f0e390b321e927b507e
wndskim <wndskim01@gmail.com>
2023-03-26 13:09:02 +0900

    Update 01_관심주.py

--- pages/01_관심주.py
+++ pages/01_관심주.py
@@ -1,10 +1,15 @@
 import streamlit as st
 from pykrx import stock
 import pandas as pd
-from datetime import date
-import GetData
+from datetime import date, datetime
+import GetData, Share
 
 st.write('관심주')
+
+
+종료일=datetime.date.today()
+시작일=Share.get_date(종료일,260*3) #3년전 날짜
+종료일=종료일.strftime('%Y%m%d')
 
 chk00=st.sidebar.checkbox('코스피200 보기',value=False)
 if chk00:
@@ -19,7 +24,7 @@ if chk00:
         for 티커 in 티커s:
             종목s.append(stock.get_market_ticker_name(티커))
         _df1=pd.DataFrame(list(zip(티커s, 종목s)), columns=['티커', '종목'])
-        _df2=GetData.load_from_prx_해당일전체(조회일)
+        _df2=GetData.load_from_pykrx_해당일전체(조회일)
         _df2=_df2[_df2.index.isin(티커s)]
 
         df=pd.merge(_df1, _df2, on='티커')
@@ -36,5 +41,13 @@ if chk00:
 
     _dict=dict(zip(종목s,티커s))
     종목=container.selectbox('선택', 종목s)
-    # container.write(종목)
-    st.write(종목, _dict[종목])
+    _티커=_dict[종목]
+
+    df_개별종목=GetData.load_from_pykrx_개별종목(시작일,종료일,_티커,)
+
+    st.dataframe(df_개별종목)
+
+
+
+
+    # st.write(종목, _dict[종목])
