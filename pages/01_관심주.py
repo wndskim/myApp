efc2309aa4df4b2e0312bd6dea97108644d71a5c
wndskim <wndskim01@gmail.com>
2023-03-27 14:46:14 +0900

    Update 01_관심주.py

--- pages/01_관심주.py
+++ pages/01_관심주.py
@@ -33,7 +33,8 @@ if chk00:
         _df1=pd.DataFrame(list(zip(티커s, 종목s)), columns=['티커', '종목'])
         _dict=dict(zip(종목s,티커s))
         st.session_state.load_state=True
-        전조회일=''
+        st.session_state.전조회일=''
+
     col1,col2,col3=st.columns([1,5,3])
     with col1:
         조회일=st.date_input('조회일', date.today())
@@ -47,7 +48,7 @@ if chk00:
         # _df1=pd.DataFrame(list(zip(티커s, 종목s)), columns=['티커', '종목'])
         # _dict=dict(zip(종목s,티커s))
 
-        if 조회일!=전조회일:
+        if 조회일!=st.session_state.전조회일:
             _df2=GetData.load_from_pykrx_해당일전체(조회일)
             _df2=_df2[_df2.index.isin(티커s)]
 
