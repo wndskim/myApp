b6af5db8ae338daadbb1c9c48c2c3c91b999e35d
wndskim <wndskim01@gmail.com>
2023-03-29 14:23:05 +0900

    Update 01_관심주.py

--- pages/01_관심주.py
+++ pages/01_관심주.py
@@ -24,15 +24,8 @@ if chk00:
     with col1:
         조회일=st.date_input('조회일', date.today())
         조회일=str(조회일).replace('-','')
-        # container=st.container()
+        container=st.container()
     with col2:
-        # 종목s=[]
-        # 티커s=stock.get_index_portfolio_deposit_file('1028')
-        # for 티커 in 티커s:
-        #     종목s.append(stock.get_market_ticker_name(티커))
-        # _df1=pd.DataFrame(list(zip(티커s, 종목s)), columns=['티커', '종목'])
-        # _dict=dict(zip(종목s,티커s))
-
         _df2=GetData.load_from_pykrx_해당일전체(조회일)
         _df2=_df2[_df2.index.isin(티커s)]
 
@@ -46,13 +39,13 @@ if chk00:
         st.write('코스피200',len(df),'건')
         st.dataframe(df)
 
-        # _종목=container.selectbox('선택', _종목s)
-        # _티커=_dict[_종목]
+        _종목=container.selectbox('선택', _종목s)
+        _티커=_dict[_종목]
 
     with col3:
-        _종목=st.selectbox('선택', _종목s)
-        _티커=_dict[_종목]
-        st.write(_종목, _티커)
+        # _종목=st.selectbox('선택', _종목s)
+        # _티커=_dict[_종목]
+        # st.write(_종목, _티커)
 
         Share.참조링크보기(_티커,_종목)
 
