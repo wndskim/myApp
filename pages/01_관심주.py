de7bbbfd8009906a6abfee3f18be93fef4be54d0
wndskim <wndskim01@gmail.com>
2023-04-04 13:30:05 +0900

    Update 01_관심주.py

--- pages/01_관심주.py
+++ pages/01_관심주.py
@@ -73,10 +73,10 @@ if chk01:
 
     st.text('파일을 읽어오지 못하는 오류 발생 ..!!')
 
-    # excel_file='/myapp/Data/차트영웅저평가.xlsx'
-    # df=pd.read_excel(excel_file)
+    excel_file='차트영웅저평가.xlsx'
+    df=pd.read_excel(excel_file)
 
-    # st.dataframe(df)
+    st.dataframe(df)
 
 
 
