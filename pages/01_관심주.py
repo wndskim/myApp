17c619eadd445217b254493e7105b21f73abc339
wndskim <wndskim01@gmail.com>
2023-04-07 11:18:28 +0900

    Update 01_관심주.py

--- pages/01_관심주.py
+++ pages/01_관심주.py
@@ -40,11 +40,11 @@ if chk00:
         st.write('코스피200',len(df),'건')
         st.dataframe(df)
 
-        _종목=container.selectbox('선택', _종목s)
+        _종목=container.selectbox('선택', _종목s,key='선택종목')
         _티커=_dict[_종목]
 
     with col3:
-        _종목=st.selectbox('선택', _종목s)
+        _종목=st.selectbox('선택', _종목s,key='참조종목')
         _티커=_dict[_종목]
         st.write(_종목, _티커)
 
