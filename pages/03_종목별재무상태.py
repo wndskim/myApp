e26851a07fc628ba534b25811b5e8e160c432876
wndskim <wndskim01@gmail.com>
2023-05-05 15:03:03 +0900

    Update 03_종목별재무상태.py

--- pages/03_종목별재무상태.py
+++ pages/03_종목별재무상태.py
@@ -6,10 +6,12 @@ st.text('종목별 2022년 재무상태')
 
 종목='삼성전자'
 df=GetData.DART_재무정보(종목)
+st.text(종목)
 st.dataframe(df)
 
 종목='자이에스앤디'
 df=GetData.DART_재무정보(종목)
+st.text(종목)
 st.dataframe(df)
 
 # df=df[df['fs_nm']=='연결재무제표']
