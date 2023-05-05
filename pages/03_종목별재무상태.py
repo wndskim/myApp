fa27a620b928e16575393313f8d9f8ccd82a3026
wndskim <wndskim01@gmail.com>
2023-05-05 15:01:18 +0900

    Update 03_종목별재무상태.py

--- pages/03_종목별재무상태.py
+++ pages/03_종목별재무상태.py
@@ -2,11 +2,16 @@ import streamlit as st
 from Codes import Share, GetData
 
 
-st.text('삼성전자 2022년 재무상태')
+st.text('종목별 2022년 재무상태')
 
-df=GetData.DART_재무정보()
+종목='삼성전자'
+df=GetData.DART_재무정보(종목)
+st.dataframe(df)
+
+종목='자이에스앤디'
+df=GetData.DART_재무정보(종목)
+st.dataframe(df)
 
 # df=df[df['fs_nm']=='연결재무제표']
 
-st.dataframe(df)
 
