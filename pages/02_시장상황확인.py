0cbbcf62d599b08f5630658b44be8f25df078b5f
wndskim <wndskim01@gmail.com>
2023-05-05 14:55:42 +0900

    Update 02_시장상황확인.py

--- pages/02_시장상황확인.py
+++ pages/02_시장상황확인.py
@@ -2,6 +2,9 @@ import streamlit as st
 import os
 from Codes import GetData, Share, Chart
 
+
+st.text(os.getenv('PATH'))
+
 st.write('시장상황 확인')
 조회일=st.sidebar.date_input('조회일')
 GetData.금감원_공시내역_보기(조회일)
