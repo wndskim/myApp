d1cf9bed581b5000f74fb9df43832e5a1bf8b997
wndskim <wndskim01@gmail.com>
2023-07-09 13:55:11 +0900

    Update 01_관심주.py

--- pages/01_관심주.py
+++ pages/01_관심주.py
@@ -21,14 +21,79 @@ def get_tickers(인덱스):
 def download_history(티커,시작일):
     return yf.download(티커+'.KS',start=시작일)['Close']
 
-def next_page_function():
+def 코스피200보기():
     if st.session_state.코스피200:
         st.session_state.코스피200 = False
         st.session_state.인덱스별 = True
 
-    elif st.session_state.인덱스별:
-        st.session_state.show_question_page = False
-        st.session_state.코스피200 = True
+        종목s=[]
+        티커s=stock.get_index_portfolio_deposit_file('1028')
+        for 티커 in 티커s:
+            종목s.append(stock.get_market_ticker_name(티커))
+        _df1=pd.DataFrame(list(zip(티커s, 종목s)), columns=['티커', '종목'])
+        _dict=dict(zip(종목s,티커s))
+
+        col1,col2,col3=st.columns([1,5,5])
+        with col1:
+            조회일=st.date_input('조회일', date.today())
+            조회일=str(조회일).replace('-','')
+            container=st.container()
+        with col2:
+            _df2=GetData.load_from_pykrx_해당일전체(조회일)
+            _df2=_df2[_df2.index.isin(티커s)]
+
+            df=pd.merge(_df1, _df2, on='티커')
+            df1=df.sort_values(by='등락률', ascending=False)
+
+            df1.reset_index(inplace=True)
+            df1.drop('index', axis=1, inplace=True)
+            _종목s=df1.종목.tolist()
+
+            st.write('코스피200 상승률순')
+            st.dataframe(df1)
+
+            _종목=container.selectbox('선택', _종목s,key='선택종목')
+            _티커=_dict[_종목]
+
+        with col3:
+            # _종목=st.selectbox('선택', _종목s,key='참조종목')
+            # _티커=_dict[_종목]
+            # st.write(_종목, _티커)
+            df2=df.sort_values(by='등락률', ascending=True)
+
+            df2.reset_index(inplace=True)
+            df2.drop('index', axis=1, inplace=True)
+            # _종목s=df1.종목.tolist()
+
+            st.write('코스피200 하락률순')
+            st.dataframe(df2)        
+
+        Share.참조링크보기(_티커,_종목)
+
+        # 개별종목 일/주/월 차트 그리기
+        df_개별종목=GetData.load_from_pykrx_개별종목(시작일,종료일,_티커)
+        df_w=df_개별종목.resample('W').agg({'시가':'first','고가':'max','저가':'min','종가':'last','거래량':'sum','거래대금':'sum'})
+        df_m=df_개별종목.resample('M',closed='right',label='right').agg({'시가':'first','고가':'max','저가':'min','종가':'last','거래량':'sum','거래대금':'sum'})
+
+        df_개별종목=GetData.set_indicator(df_개별종목)
+        df_개별종목.reset_index(inplace=True)
+        df_w=GetData.set_indicator(df_w)
+        df_w.reset_index(inplace=True)
+        df_m=GetData.set_indicator(df_m)
+        df_m.reset_index(inplace=True)
+
+        Chart.차트_일봉(df_개별종목[-150:],_종목)
+        Chart.차트_주봉(df_w,_종목)
+        Chart.차트_월봉(df_m,_종목)
+
+
+    return
+
+def 인덱스별보기():
+    st.session_state.show_question_page = False
+    st.session_state.코스피200 = True
+
+    st.write('인덱스별보기')
 
     # elif st.session_state.show_answer_page:
     #     print("increase count")
@@ -43,17 +108,19 @@ def next_page_function():
     #     st.session_state.show_submit_page = False
     #     st.session_state.show_end_page = True
 
+    return
+
 key1,key2,key3,key4='key1','key2','key3','key4'
 if "코스피200" not in st.session_state:
     st.session_state.코스피200 = True
 if "인덱스별" not in st.session_state:
     st.session_state.인덱스별= False
 
+_, col2, _ = st.columns([1, 1, 1])
 if st.session_state.코스피200:
     st.markdown("<h1 style='text-align: center; color: white;'>This quizz will test your knowledge about some of the most random facts ever.</h1>", unsafe_allow_html=True)
     st.markdown("<h2 style='text-align: center; color: white;'>Hope you are ready!</h1>", unsafe_allow_html=True)
     st.markdown("##")
-    _, col2, _ = st.columns([1, 1, 1])
 
     # option = col2.selectbox('Number of facts', (str(int(len(st.session_state.ids)/10)),
     #                                             str(int(len(st.session_state.ids)/4)),
@@ -82,7 +149,10 @@ if st.session_state.코스피200:
         text-decoration:none;
     }
     </style>""", unsafe_allow_html=True)
-    col2.button("Start test!", on_click=next_page_function, key=key1)
+    col2.button("코스피200보기", on_click=코스피200보기, key=key1)
+else:
+    col2.button("인덱스별보기", on_click=인덱스별보기, key=key2)
+
 
     
 
