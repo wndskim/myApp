e17a1af9f80fac511c99fcfa5252a92364a34b79
wndskim <wndskim01@gmail.com>
2023-12-21 13:12:31 +0900

    Update GetData.py

--- Codes/GetData.py
+++ Codes/GetData.py
@@ -9,6 +9,8 @@ def load_from_yfinance():
 
     tickers=['SCHD','TQQQ','TMF','PFIX']
     df=yf.download(tickers,start='2013-01-01',end='2023-12-20').Close
+    for ticker in tickers:
+        df[ticker+'sma5']=ta.trend.sma_indicator(df.ticker, window=120)
 
 
     return df
