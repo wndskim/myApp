3888e803c88468992766b1cced0c4e03e2d2b352
wndskim <wndskim01@gmail.com>
2023-12-21 11:45:54 +0900

    Update MSMS.py

--- MSMS.py
+++ MSMS.py
@@ -8,11 +8,21 @@ def main():
     st.header('My Stock Management System(MSMS)')
 
     st.write('심플강력 전략')
-    st.text('1. 투자ETFs')
+    st.write('1. 투자ETFs')
     st.text(' - TQQQ: 50%')
     st.text(' - SCHD: 30%')
     st.text(' - PFIX: 10%')
     st.text(' - TMF: 10%')
+    st.write('2. Rebalancing')
+    st.text(' - 매월 첫 거래일 비중을 맞추어 리밸런싱 진행')
+    st.text(' - SCHD는 리밸런싱 하지 않는다')
+    st.text(' - 200일선 아래에 있는 자산은 50% 매도')
+    st.text(' - 140일선이 하락하고 있는 자산은 50% 추가 매도(이동평균선 하락은 전전날과 전날 이평선을 비교하여 판단)')
+    st.text(' - 감산에서 2연타를 맞은 자산이 PFIX나 TMF인경우 비중을 0으로 만들고 모두 매도')
+    st.text(' - TMF를 모두 매도 했다면 PFIX를 2배의 비중으로 보유')
+    st.text(' - PFIX를 모두 매도 했다면 TMF를 2배의 비중으로 보유')
+    st.text(' - TQQQ가 감산 2연타를 맞으면 PFIX에 35%의 비중을 더해 준다')
+    st.text(' - PFIX를 모두 매도 했는데 TQQQ가 2연타 감산을 맞지 않은 상태면 남은 현금으로 TQQQ를 매수')
     
 
 
